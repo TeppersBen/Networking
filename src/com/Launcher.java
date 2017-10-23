6589faa12cc63ff33fb4a087811de803c5e0fd50
TeppersBen <Ben.Teppers@student.pxl.be>
2017-10-23 21:21:21 +0200

    Launcher version upgrade

--- src/com/Launcher.java
+++ src/com/Launcher.java
@@ -6,8 +6,8 @@ public class Launcher {
 
 	public static void main(String[] args) {
 		Settings.debug = true;
-		Settings.versionRelease = "0.03.0022.050";
-		new WindowBuilder(0, 4, 52);
+		Settings.versionRelease = "0.04.0023.053";
+		new WindowBuilder(0, 4, 53);
 	}
 	
 }
