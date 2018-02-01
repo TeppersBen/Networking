8a9893d97c8bbbf466f889e278286363b6c50674
TeppersBen <Ben.Teppers@student.pxl.be>
2018-02-01 17:23:58 +0100

    New UI component added: TextArea
    for C# users:TextArea is a object with the same functionality as TextBox
    in C#.

---
+++ src/com/engine/components/TextArea.java
@@ -0,0 +1,51 @@
+package com.engine.components;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+
+import javax.swing.BorderFactory;
+import javax.swing.JTextArea;
+
+public class TextArea extends JTextArea {
+
+	private static final long serialVersionUID = 6715744721635848701L;
+
+	private String filterName;
+	
+	public TextArea() {
+		this("");
+	}
+	
+	public TextArea(String filter) {
+		filterName = " " + filter;
+		createFieldFocusUtility();
+		setFont(new Font("Tahoma", 0, 11));
+		setBorder(BorderFactory.createLineBorder(Color.lightGray));
+		setWrapStyleWord(true);
+		setLineWrap(true);
+	}
+	
+	private void createFieldFocusUtility() {
+		setForeground(Color.GRAY);
+		setText(filterName);
+		addFocusListener(new FocusListener() {
+		    @Override
+		    public void focusGained(FocusEvent e) {
+		        if (getText().equals(filterName)) {
+		        	setText("");
+		        	setForeground(Color.BLACK);
+		        }
+		    }
+		    @Override
+		    public void focusLost(FocusEvent e) {
+		        if (getText().isEmpty()) {
+		        	setForeground(Color.GRAY);
+		        	setText(filterName);
+		        }
+		    }
+		});
+	}
+	
+}
