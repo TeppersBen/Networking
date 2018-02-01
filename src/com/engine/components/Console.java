b66fa2ec69cf4d8830a581946e92469229edbb2c
TeppersBen <Ben.Teppers@student.pxl.be>
2018-02-01 17:26:55 +0100

    new UI component which reacts as a PacketTracer Prompt

---
+++ src/com/engine/components/Console.java
@@ -0,0 +1,33 @@
+package com.engine.components;
+
+import java.awt.Font;
+
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+
+public class Console extends JScrollPane {
+
+	private static final long serialVersionUID = 8151290794790772312L;
+
+	private JTextArea txtarea;
+	
+	public Console() {
+		this(11);
+	}
+	
+	public Console(int textSize) {
+		txtarea = new JTextArea();
+		txtarea.setEnabled(false);
+		setViewportView(txtarea);
+		txtarea.setFont(new Font("Monospaced", Font.PLAIN, textSize));
+		txtarea.setText("[... Waiting for prompt action ...]");
+	}
+	
+	public void clearText() {
+		txtarea.setText("");
+	}
+	
+	public void println(String line) {
+		txtarea.append(line + "\n");
+	}
+}
