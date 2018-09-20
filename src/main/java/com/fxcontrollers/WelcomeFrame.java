3405c093671d7123e9ae81372a50750e1ef88519
Ben Teppers <ben.teppers@icloud.com>
2018-09-20 13:35:53 +0200

    DEVICE SWITCH --WelcomeFrame preparation

--- src/main/java/com/fxcontrollers/WelcomeFrame.java
+++ src/main/java/com/fxcontrollers/WelcomeFrame.java
@@ -1,4 +1,25 @@
 package com.fxcontrollers;
 
+import com.engine.handlers.LanguageHandler;
+import com.jfoenix.controls.JFXButton;
+import javafx.fxml.FXML;
+
 public class WelcomeFrame {
+    @FXML private JFXButton button_converters;
+    @FXML private JFXButton button_vlsm_calculator;
+
+    @FXML private void initialize() {
+        System.out.println(getClass().getSimpleName() + ": initializing");
+        button_converters.setText(new LanguageHandler().getKey("tab_calculators_converter"));
+        button_vlsm_calculator.setText(new LanguageHandler().getKey("tab_calculators_vlsm"));
+    }
+
+    @FXML private void go_to_converters_frame() {
+        System.out.println("Go to converters frame");
+    }
+
+    @FXML private void go_to_vlsm_calculator_frame() {
+        System.out.println("Go to vlsm calculator frame");
+    }
+
 }
